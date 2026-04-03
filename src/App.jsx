f10490757eb14256ea0b48174db594a7387ff47e
Mohammad Kaif <developer.kaifsaifi@gmail.com>
2026-04-04 01:08:42 +0530

    debugged successfull

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
 
-// import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-// import HomeLayout from './HomeLayout'
-// import ContactPage from './pages/ContactPage'
-// import ProjectPage from './pages/ProjectPage'
-// import ServicePage from './pages/ServicePage'
-// import MainPage from './pages/MainPage'
-// import ScrollToTop from './components/ScrollToTop'
+import HomeLayout from './HomeLayout'
+import ContactPage from './pages/ContactPage'
+import ProjectPage from './pages/ProjectPage'
+import ServicePage from './pages/ServicePage'
+import MainPage from './pages/MainPage'
+import ScrollToTop from './components/ScrollToTop'
 
-
-// export default function App() {
-//   return (
-//     <>
-//     <ScrollToTop />
-//     <Routes>
-//       {/* Layout Route */}
-   
-       
-//       <Route path="/" element={<HomeLayout />}>
-//         <Route index element={<MainPage/>} />
-//         <Route path="services" element={<ServicePage/>} />
-//         <Route path="projects" element={<ProjectPage/>} />
-//         <Route path="contact" element={<ContactPage/>} />
-//       </Route>
-//     </Routes>
-//     </>
-// )
-// }
-
-import React from 'react'
 
 export default function App() {
   return (
-    <div>Hello</div>
-  )
+    <>
+    <ScrollToTop />
+    <Routes>
+      {/* Layout Route */}
+   
+       
+      <Route path="/" element={<HomeLayout />}>
+        <Route index element={<MainPage/>} />
+        <Route path="services" element={<ServicePage/>} />
+        <Route path="projects" element={<ProjectPage/>} />
+        <Route path="contact" element={<ContactPage/>} />
+      </Route>
+    </Routes>
+    </>
+)
 }
+
