085a19a8956ad124ad65372378cb1f7f84455d7a
Mohammad Kaif <developer.kaifsaifi@gmail.com>
2026-04-04 01:02:39 +0530

    _

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
-import { Route, Routes } from 'react-router-dom'
+// import { Route, Routes } from 'react-router-dom'
 
-import HomeLayout from './HomeLayout'
-import ContactPage from './pages/ContactPage'
-import ProjectPage from './pages/ProjectPage'
-import ServicePage from './pages/ServicePage'
-import MainPage from './pages/MainPage'
-import ScrollToTop from './components/ScrollToTop'
+// import HomeLayout from './HomeLayout'
+// import ContactPage from './pages/ContactPage'
+// import ProjectPage from './pages/ProjectPage'
+// import ServicePage from './pages/ServicePage'
+// import MainPage from './pages/MainPage'
+// import ScrollToTop from './components/ScrollToTop'
 
 
 // export default function App() {
