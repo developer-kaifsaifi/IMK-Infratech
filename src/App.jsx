2f5889dd679c721efa3f1b9489edbd6e21f95eb5
Mohammad Kaif <developer.kaifsaifi@gmail.com>
2026-04-12 00:00:07 +0530

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,48 @@ import MainPage from './pages/MainPage'
 import ScrollToTop from './components/ScrollToTop'
 import { useEffect, useState } from 'react'
 import Loader from './components/Loader'
+import { images } from './imageList'
+
+
+
+const preloadImages = (imageUrls) => {
+  return Promise.all(
+    imageUrls.map((url) => {
+      return new Promise((resolve) => {
+        const img = new Image()
+        img.src = url
+        img.onload = resolve
+        img.onerror = resolve
+      })
+    })
+  )
+}
+
+
 
 
 export default function App() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
+  
+ useEffect(() => {
+    const startTime = Date.now()
 
-  useEffect(() => {
-    // simulate loading (API ya assets load hone tak)
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+    preloadImages(images).then(() => {
+      const elapsed = Date.now() - startTime
+      const minTime = 1000 // 👈 1 sec
 
+      const remainingTime = minTime - elapsed
 
-  return (   <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
+      setTimeout(() => {
+        setLoading(false)
+      }, remainingTime > 0 ? remainingTime : 0)
+    })
+  }, [])
+  if (loading) {
+    return (<Loader/>)
+  }
+
+  return (<>
     <ScrollToTop />
     <Routes>
       {/* Layout Route */}
@@ -39,10 +63,41 @@ export default function App() {
         <Route path="contact" element={<ContactPage/>} />
       </Route>
     </Routes>
-    </>
-      )}
-    </>
-    
-)
+    </>)
 }
+
+// export default function App() {
+//     const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     // simulate loading (API ya assets load hone tak)
+//     setTimeout(() => {
+//       setLoading(false);
+//     }, 2000);
+//   }, []);
+
+
+//   return (   <>
+//       {loading ? (
+//         <Loader />
+//       ) : (<>
+//     <ScrollToTop />
+//     <Routes>
+//       {/* Layout Route */}
+   
+       
+//       <Route path="/" element={<HomeLayout />}>
+//         <Route index element={<MainPage/>} />
+//         <Route path="services" element={<ServicePage/>} />
+//         <Route path="projects" element={<ProjectPage/>} />
+//         <Route path="contact" element={<ContactPage/>} />
+//       </Route>
+//     </Routes>
+//     </>
+        
+//       )}
+//     </>
+    
+// )
+// }
 
---
+++ src/imageList.js
@@ -0,0 +1,6 @@
+export const images = [
+  "/assets/crane.png",
+  "/assets/imk21re.png",
+  "/assets/beam.png",
+  "/assets/worker.png"
+]
